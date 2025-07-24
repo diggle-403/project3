36fd09093195c1bce89cd896f4be288365ea3c3a
diggle-403 <dylan.huston.official@gmail.com>
2025-07-24 02:32:12 -0500

    Add files via upload

--- src/MultPanel.java
+++ src/MultPanel.java
@@ -9,6 +9,9 @@ public class MultPanel extends JFrame implements ActionListener {
     private static final int height = 1080;
     private CardLayout cardLayout;
     private JPanel cardPanel;
+    private int temper = 0;
+    private int numOnes = 0;
+    private int numTwos = 0;
 
 
     /**
@@ -32,6 +35,8 @@ public class MultPanel extends JFrame implements ActionListener {
         cardPanel.add(threatenPanel(), "threaten");
         cardPanel.add(choicesPanel(), "choices");
         cardPanel.add(helpPanel(), "help");
+        cardPanel.add(theEnd(), "end");
+        cardPanel.add(calculatorPanel(), "calculator");
 
         topFrame.add(cardPanel);
         topFrame.setVisible(true);
@@ -44,20 +49,28 @@ public class MultPanel extends JFrame implements ActionListener {
         JPanel panel1 = new JPanel(null);
         panel1.setBackground(Color.GRAY);
 
-        JLabel titleLabel = new JLabel("What would you like to do?");
+
+        String query = "What would you like to do?";
+        int queryLength = query.length();
+        JLabel titleLabel = new JLabel(query);
         titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
-        titleLabel.setBounds(800, 540, 1000,  20);
+        titleLabel.setBounds(800 - queryLength, 540, 1000,  50);
         panel1.add(titleLabel);
 
         JButton happyButton = new JButton("Be Happy :)");
-        happyButton.setBounds(0, 1000, 100, 30);
+        happyButton.setBounds(800, 1000, 100, 30);
         happyButton.addActionListener(e -> cardLayout.show(cardPanel, "happy"));
         panel1.add(happyButton);
 
         JButton sadButton = new JButton("Be Sad :(");
-        sadButton.setBounds(150, 1000, 100, 30);
+        sadButton.setBounds(900, 1000, 100, 30);
         sadButton.addActionListener(e -> cardLayout.show(cardPanel, "sad"));
         panel1.add(sadButton);
+
+        JButton calcButton = new JButton("Calculator");
+        calcButton.setBounds(1000, 1000, 100, 30);
+        calcButton.addActionListener(e -> {cardLayout.show(cardPanel, "calculator");});
+        panel1.add(calcButton);
 
 
         return panel1;
@@ -67,24 +80,26 @@ public class MultPanel extends JFrame implements ActionListener {
 
         int centerX = 1920/2;
         int centerY = 1080/2;
+        String title = "You found a Kitty!";
+        int titleLength = title.length();
 
         JPanel panel2 = new JPanel(null);
         panel2.setBackground(Color.GRAY);
-        JLabel titleLabel = new JLabel("You Found a Kitty!");
+        JLabel titleLabel = new JLabel(title);
         titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
-        titleLabel.setBounds(centerX, 10, 1000,  20);
+        titleLabel.setBounds(centerX - titleLength, 10, 1000,  30);
         panel2.add(titleLabel);
 
 
         ImageIcon kittyIcon = new ImageIcon("kitty.jpg");
         JLabel kittyLabel = new JLabel(kittyIcon);
-        kittyLabel.setBounds(600, 200, 600,  600);
+        kittyLabel.setBounds(550, 200, 600,  600);
         panel2.add(kittyLabel);
 
 
         JButton happyButton = new JButton("Yay!");
         happyButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
-        happyButton.setBounds(0,1000,100,30);
+        happyButton.setBounds(800,1000,100,30);
         panel2.add(happyButton);
         happyButton.addActionListener(e -> cardLayout.show(cardPanel, "top"));
 
@@ -99,6 +114,74 @@ public class MultPanel extends JFrame implements ActionListener {
     }
 
 
+    private JPanel calculatorPanel(){
+
+        int sum = 0;
+        JPanel calcPanel = new JPanel(null);
+        calcPanel.setBackground(Color.GRAY);
+
+        JButton calcButton = new JButton("Calculate");
+        calcButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        calcButton.setBounds(800,0,300,30);
+
+        calcPanel.add(calcButton);
+
+        JButton backButton = new JButton("Back");
+        backButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        backButton.setBounds(0,1000,100,30);
+        backButton.addActionListener(e -> {cardLayout.show(cardPanel, "top");});
+        calcPanel.add(backButton);
+
+        JButton oneButton = new JButton("One");
+        oneButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        oneButton.setBounds(800,600,100,60);
+
+        JButton twoButton = new JButton("Two");
+        twoButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        twoButton.setBounds(900,600,100,60);
+
+        JButton plusButton = new JButton("+");
+        plusButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        plusButton.setBounds(600,600,100,60);
+
+        calcPanel.add(oneButton);
+        calcPanel.add(twoButton);
+        calcPanel.add(plusButton);
+
+        
+
+        calcButton.addActionListener(e -> {
+
+            JFrame outSum = new JFrame();
+            outSum.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            outSum.setSize(300,300);
+            outSum.setResizable(false);
+            outSum.setLocationRelativeTo(null);
+            outSum.setVisible(true);
+
+            JPanel outPanel = new JPanel(null);
+            JLabel outSumLabel = new JLabel("SUM");
+            outSumLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+            outSumLabel.setBounds(147,50,50,50);
+
+
+            int totalSum = numOnes + numTwos;
+            String totalString = Integer.toString(totalSum);
+            JLabel totalSumLabel = new JLabel(totalString);
+            totalSumLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+            totalSumLabel.setBounds(150 - totalString.length(),150,40,40);
+            outPanel.add(totalSumLabel, BorderLayout.SOUTH);
+            outPanel.add(outSumLabel, BorderLayout.NORTH);
+
+            outSum.setContentPane(outPanel);
+
+
+        });
+
+        oneButton.addActionListener(e -> {numOnes++;});
+        twoButton.addActionListener(e -> {numTwos = numTwos+2;});
+    return calcPanel;
+    }
 
 
     private JPanel sadPanel(){
@@ -107,7 +190,7 @@ public class MultPanel extends JFrame implements ActionListener {
         JPanel sadPanel = new JPanel(null);
         sadPanel.setBackground(Color.GRAY);
         JLabel titleLabel = new JLabel("You Died :(");
-        titleLabel.setBounds(960, 10, 1000,  20);
+        titleLabel.setBounds(800, 10, 1000,  30);
         titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
         sadPanel.add(titleLabel);
 
@@ -120,13 +203,13 @@ public class MultPanel extends JFrame implements ActionListener {
 
 
         JButton sadButton = new JButton("Darn");
-        sadButton.setBounds(0,1000,100,30);
+        sadButton.setBounds(750,1000,100,30);
         sadPanel.add(sadButton);
         sadButton.addActionListener(e -> cardLayout.show(cardPanel, "top"));
 
 
         JButton answerButton = new JButton("What does this mean?");
-        answerButton.setBounds(500,1000,300,30);
+        answerButton.setBounds(900,1000,300,30);
         sadPanel.add(answerButton);
         answerButton.addActionListener(e -> cardLayout.show(cardPanel, "death"));
 
@@ -257,6 +340,9 @@ public class MultPanel extends JFrame implements ActionListener {
     }
 
     JPanel helpPanel() {
+
+
+
         JPanel helpPanel = new JPanel(null);
         helpPanel.setBackground(Color.BLACK);
 
@@ -265,69 +351,53 @@ public class MultPanel extends JFrame implements ActionListener {
         helpBox.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
         helpBox.setBackground(Color.BLACK);
         helpBox.setForeground(Color.WHITE);
-        helpBox.setBounds(600, 200, 800,  600);
+        helpBox.setBounds(800, 200, 800,  100);
+        helpBox.setEditable(false);
 
         JButton submitButton = new JButton("Submit");
         submitButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
-        submitButton.setBounds(600,1000,500,30);
+        submitButton.setBounds(800,900,500,30);
         submitButton.setBackground(Color.BLACK);
         submitButton.setForeground(Color.WHITE);
 
 
         JTextArea creditCard = new  JTextArea("Credit Card");
+        creditCard.setEditable(false);
         creditCard.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
         creditCard.setBackground(Color.BLACK);
         creditCard.setForeground(Color.WHITE);
-        creditCard.setBounds(600, 500, 800,  600);
+        creditCard.setBounds(600, 350, 800,  30);
+        helpPanel.add(creditCard);
 
-        JTextField creditCardNumber = new  JTextField("Credit Card Number");
-        creditCardNumber.setBounds(600, 200, 200,  20);
+        JTextField creditCardNumber = new  JTextField();
+        creditCardNumber.setBounds(600, 400, 300,  20);
         creditCardNumber.setBackground(Color.LIGHT_GRAY);
         creditCardNumber.setForeground(Color.BLACK);
         helpPanel.add(creditCardNumber);
 
-        JTextField socialSecurityNumber = new  JTextField("Social Security Number");
-        socialSecurityNumber.setBounds(600, 300, 200,  20);
+        JTextArea socialNum = new  JTextArea("Social Security Number");
+        socialNum.setEditable(false);
+        socialNum.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        socialNum.setBackground(Color.BLACK);
+        socialNum.setForeground(Color.WHITE);
+        socialNum.setBounds(600, 450, 800,  30);
+        helpPanel.add(socialNum);
+
+        JTextField socialSecurityNumber = new  JTextField();
+        socialSecurityNumber.setBounds(600, 500, 300,  20);
         socialSecurityNumber.setBackground(Color.LIGHT_GRAY);
         socialSecurityNumber.setForeground(Color.BLACK);
         helpPanel.add(socialSecurityNumber);
 
 
-        submitButton.addActionListener(e -> {
 
+
+        submitButton.addActionListener(e -> {
 
             String creditCardNumberString = creditCardNumber.getText();
             String socialSecurityNumberString = socialSecurityNumber.getText();
 
             if (creditCardNumberString.length() != 16 || socialSecurityNumberString.length() != 9) {
-
-                JFrame flashFrame = new JFrame();
-                JPanel flashPanel = new JPanel();
-                flashFrame.setContentPane(flashPanel);
-
-                new Timer(200, e1 -> {
-
-                    int flash = 0;
-                    boolean flashType = true;
-                    if(flash >= 10)
-                    {
-                        return;
-                    }
-
-                    if(flashType)
-                    {
-                        flashPanel.setBackground(Color.RED);
-
-                    }
-                    else {
-                        flashPanel.setBackground(Color.BLACK);
-                    }
-                    flash++;
-                    
-
-                });
-
-                helpPanel.setBackground(Color.BLACK);
 
                 JFrame error = new  JFrame("Silly");
                 error.setVisible(true);
@@ -336,15 +406,36 @@ public class MultPanel extends JFrame implements ActionListener {
                 error.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
                 JLabel popup = new JLabel("That's not right silly!");
+                popup.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+                popup.setLocation(200,200);
+                temper++;
 
                 error.add(popup);
+
+                if(temper >= 3)
+                {
+
+                    JFrame angryFrame = new  JFrame();
+                    angryFrame.setVisible(true);
+                    angryFrame.setResizable(false);
+                    angryFrame.setSize(400,400);
+                    angryFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+                    JPanel angryPanel = new  JPanel(new BorderLayout());
+                    ImageIcon angryIcon = new ImageIcon("angry.jpg");
+                    JLabel angryLabel = new JLabel(angryIcon);
+
+                    angryPanel.add(angryLabel, BorderLayout.CENTER);
+
+                    angryFrame.setContentPane(angryPanel);
+
+                }
 
             }
 
             else {
-                cardLayout.show(cardPanel, "top");
+                cardLayout.show(cardPanel, "end");
             }
-
 
         });
         helpPanel.add(submitButton);
@@ -352,6 +443,58 @@ public class MultPanel extends JFrame implements ActionListener {
         return helpPanel;
     }
 
+
+    JPanel theEnd()
+    {
+        JPanel theEndPanel = new JPanel(new BorderLayout(30, 30));
+
+        JLabel theEndLabel = new JLabel("Was it really worth it?");
+        theEndLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        theEndPanel.setBackground(Color.BLACK);
+        theEndPanel.setForeground(Color.WHITE);
+        theEndPanel.add(theEndLabel, BorderLayout.NORTH);
+
+        JButton exitButton = new JButton("Dude did you just steal my info?");
+        exitButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        exitButton.setBackground(Color.BLACK);
+        exitButton.setForeground(Color.WHITE);
+        theEndPanel.add(exitButton, BorderLayout.SOUTH);
+
+        JButton leaveButton = new JButton("Yes");
+        leaveButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        leaveButton.setBackground(Color.BLACK);
+        leaveButton.setForeground(Color.WHITE);
+        theEndPanel.add(leaveButton, BorderLayout.EAST);
+
+        leaveButton.addActionListener(e -> {cardLayout.show(cardPanel, "happy");});
+
+
+
+        exitButton.addActionListener(e -> {
+
+           JFrame theftFrame = new  JFrame();
+           theftFrame.setVisible(true);
+           theftFrame.setResizable(false);
+           theftFrame.setSize(1920,1080);
+           theftFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+           JPanel theftPanel = new  JPanel(new BorderLayout());
+
+           JLabel theftLabel = new JLabel("YES");
+           theftLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 80));
+           theftPanel.add(theftLabel, BorderLayout.NORTH);
+
+           theftFrame.setContentPane(theftPanel);
+
+
+        });
+
+
+
+
+
+        return theEndPanel;
+    }
 
 
     @Override
